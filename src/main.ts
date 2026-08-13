9c750c0dce76bc2106edbb35ae1a7d95792b27a9
lingxin <173931019+Ling0727-ai@users.noreply.github.com>
2026-08-13 21:45:40 +0800

    Enhance loading experience with CSS styles and JavaScript for 404 and index pages

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import './styles.css'
 
 createApp(App).mount('#app')
+document.documentElement.classList.remove('js-loading')
